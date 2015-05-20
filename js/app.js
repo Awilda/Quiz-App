309f1c44a9f7cea6a6de5752099f0dbffe936136
Awilda <awildacantres@gmail.com>
2015-05-20 17:41:46 -0400

    Update

--- js/app.js
+++ js/app.js
@@ -1,40 +1,49 @@
-$(document).ready(function(){
-	$("#submit").click(function(){
+
 
 
 //quiz questions//
-var questions =  [
-	{
-	question: "What does a cat spend a third of its time awake doing?",
-	choices:["Eating", "Cleaning itself", "Playing", "Watching TV"],
-	correctAnswer: "Cleaning itself"},
-	{
-	question: "Who created the cat 'flap' door?",
-	choices: ["Albert Einstein", "Nicolaus Copernicus", "Sir Isaac Newton", "Galileo Galilei"],
-	correctAnswer: "Sir Issac Newton"},
-	{
-	question: "Female cats are also known as:",
-	choices: ["Queen, Molly", "Whiskers, Baby", "Princess, Holly", "Nena, Juliet"],
-	correctAnswer: "Queen, Molly"},
-	{
-	question: "A human fingerprint is to a cat's:",
-	choices: ["Nose", "Paws", "Nails", "Whiskers"],
-	correctAnswer: "Nose"},
-	{
-	question: "Approximately, how long is a cat's pregnancy?",
-	choices: ["6 weeks", "9 months", "6 months", "9 weeks"],
-	correctAnswer: "9 weeks"}
-];
+var questions = 
 
-		if(questions.getElementByName("choices") === true){
-			question++;
-		} else {
-			alert("Try Again!");
-			return;
-		}
+	'Who created the cat \"flap\" door?<br>'
+		'<input type="radio" name = "answer" value = "Albert Einstein">Albert Einstein<br>'
+		'<input type="radio" name = "answer" value = "Nicolaus Copernicus">Nicolaus Copernicus<br>'
+		'<input type="radio" name = "answer" value = "Sir Isaac Newton">Sir Isaac Newton<br>'
+		'<input type="radio" name = "answer" value = "Galileo Galilei">Galileo Galilei<br>' ,
+	
+	'Female cats are also known as:<br>'
+		'<input type="radio" name = "answer" value = "Queen, Molly">Queen, Molly<br>'
+		'<input type="radio" name = "answer" value = "Whiskers, Baby">Whiskers, Baby<br>'
+		'<input type="radio" name = "answer" value = "Princess, Holly">Princess, Holly<br>'
+		'<input type="radio" name = "answer" value = "Nena, Juliet">Nena, Juliet<br>' ,
+	
+	'A human fingerprint is to a cat\'s:<br>' 
+		'<input type="radio" name = "answer" value = "Nose">Nose<br>'
+		'<input type="radio" name = "answer" value = "Paws">Paws<br>'
+		'<input type="radio" name = "answer" value = "Nails">Nails<br>'
+		'<input type="radio" name = "answer" value = "Whiskers">Whiskers<br>' ,
+	
+	'Approximately, how  \"long\" + is a cat\'s pregnancy?<br>'
+		'<input type="radio" name = "answer" value = "6 weeks">6 weeks<br>'
+		'<input type="radio" name = "answer" value = "9 months">9 months<br>'
+		'<input type="radio" name = "answer" value = "6 months">6 months<br>'
+		'<input type="radio" name = "answer" value = "9 months">9 months<br>'
+	
+;
+
+
+
+$('#questionnaire').html(questions[0])
+var currentQuestion = 0;
+
+$(document).ready(function(){
+
+		$('#questionnaire').submit(function(){
+			currentQuestion++;
+			$('#questionnaire').html(questions[currentQuestion]);
+			return false;
+		});
 	
 	
-	});
 });
 
 
